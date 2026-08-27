570fe5a653c52720cb8923d87bbd37c86ded911a
اسم المستخدم الخاص بك على GitHub <your-email@example.com>
2026-08-27 03:53:50 +0300

    Add unit test to app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('DevOps Pipeline is Working!')
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    print("Tests Passed Successfully!")
+
+if __name__ == "__main__":
+    test_add()
