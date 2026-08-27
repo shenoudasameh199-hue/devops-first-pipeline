d6da6cf65b8ca8089e1d3fdc27075089661adeb5
اسم المستخدم الخاص بك على GitHub <your-email@example.com>
2026-08-27 04:29:50 +0300

    Fix security scan assertion issue

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 def add(a, b):
     return a + b
 
-def test_add():
-    assert add(2, 3) == 5
-    print("Tests Passed Successfully!")
-
 if __name__ == "__main__":
-    test_add()
+    result = add(2, 3)
+    if result == 5:
+        print("Tests Passed Successfully!")
+    else:
+        raise ValueError("Test failed!")
